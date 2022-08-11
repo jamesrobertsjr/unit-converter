b8f8430478ca20c69ff62b398e292f443510496c
jameserv <106282058+jameserv@users.noreply.github.com>
2022-08-11 19:37:21 -0400

    Adjusted formatting.

--- temperatureConverter.cpp
+++ temperatureConverter.cpp
@@ -21,14 +21,12 @@ double fahrenheitToCelsius(double);
 double celsiusToFahrenheit(double);
 char validateRunAgain (char);
 
-int main() 
-{
+int main() {
     char conversionSelection = '\0';
     double ftemp = 0.0, ctemp = 0.0;
     char yesNo = '\0';
 
-    do
-    {
+    do {
         // Displays options to user.
         cout << "(A) - Convert Fahrenheit to Celsius\n"; 
         cout << "(B) - Convert Celsius to Fahrenheit\n";
@@ -36,20 +34,19 @@ int main()
         cin >> conversionSelection;
 
         // Converts user entry to capital letters, then feeds it into switch statement.
-        switch (toupper(conversionSelection))
-        {
+        switch (toupper(conversionSelection)) {
             case f_to_c:
                 cout << "Enter a temperature value in Fahrenheit: ";
                 cin >> ftemp;
-                cout << "Converted to Celsius: " << fahrenheitToCelsius(ftemp) << "ºC" << endl;
+                cout << "Converted to Celsius: ";
+                cout << fahrenheitToCelsius(ftemp) << "ºC" << endl;
                 break;
-
             case c_to_f:
                 cout << "Enter a temperature value in Celsius: ";
                 cin >> ctemp;
-                cout << "Converted to Fahrenheit: " << celsiusToFahrenheit(ctemp) << "ºF" << endl;
+                cout << "Converted to Fahrenheit: "; 
+                cout << celsiusToFahrenheit(ctemp) << "ºF" << endl;
                 break;
-        
             default:
                 cout << USER_INPUT_ERROR << endl;
                 break;
@@ -63,26 +60,22 @@ int main()
     return 0;
 }
 
-double fahrenheitToCelsius(double ftemp) 
-{
+double fahrenheitToCelsius(double ftemp) {
     double celsius = (ftemp - 32.0) * (5.0 / 9.0);
     return celsius;
 }
 
-double celsiusToFahrenheit(double ctemp) 
-{
+double celsiusToFahrenheit(double ctemp) {
     double fahrenheit = (ctemp * (9.0 / 5.0)) + 32.0;
     return fahrenheit;
 }
 
 // Validates user entry for re-running program.
-char validateRunAgain (char yesNo) 
-{
-    while (toupper(yesNo) != 'Y' && toupper(yesNo) != 'N') 
-    {
+char validateRunAgain (char yesNo) {
+    while (toupper(yesNo) != 'Y' && toupper(yesNo) != 'N') {
         cout << USER_INPUT_ERROR << endl;
         cin >> yesNo;
     }
-    
+
     return toupper(yesNo);
 }
